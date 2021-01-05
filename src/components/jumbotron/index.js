5f9629263e6862c43d290428955f8ab4450ca9e5
Diwo777 <diego.zegarra79@icloud.com>
2021-01-04 20:36:07 -0500

    added first component

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Inner, Container } from './styles/jumbotron';
+
+export default function Jumbotron({ children, direction = 'row', ...restProps}) {
+    return (
+    <Inner direction={direction}>
+      <p> Hello again! </p>  
+    </Inner>)
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps}) {
+    return <Container { ...restProps}>{children}</Container>
+}
