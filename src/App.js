ccbd2731bbb51ed20d7543e4529c54c24750ad84
Diwo777 <diego.zegarra79@icloud.com>
2021-01-07 11:53:02 -0500

    added css

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
       {jumboData.map((item) => (
         <Jumbotron key={item.id} direction={item.direction}>
           <Jumbotron.Pane> 
-           <Jumbotron.title> {item.title} </Jumbotron.title>
-           <Jumbotron.subTitle> {item.subTitle} </Jumbotron.subTitle>
+           <Jumbotron.Title> {item.title} </Jumbotron.Title>
+           <Jumbotron.SubTitle> {item.subTitle} </Jumbotron.SubTitle>
           </Jumbotron.Pane>
           <Jumbotron.Pane> 
            <Jumbotron.Image src={item.image} alt={item.alt}/> 
