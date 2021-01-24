4e70165f9cd8ff5946bcc834f1c230b81baeb193
Diwo777 <diego.zegarra79@icloud.com>
2021-01-24 16:25:13 -0500

    added footer

--- src/components/opt-form/index.js
+++ src/components/opt-form/index.js
@@ -8,4 +8,10 @@ OptForm.Input = function OptFormInput({ ...restProps }) {
     return <Input { ...restProps } />;
 };
 
-OptForm.Button = function OptFormButton( { children, ...restProps})
+OptForm.Button = function OptFormButton( { children, ...restProps}) {
+    return ( 
+        <Button { ...restProps}>
+            {}
+        </Button>
+    )
+}
