0031b3e1245e495cf7d4e8041602133565521310
Diwo777 <diego.zegarra79@icloud.com>
2021-02-02 11:38:25 -0500

    Added Browser Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Router, Switch } from 'react-router-dom'
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
 import * as ROUTES from './constants/routes'
 
 export default function App() {
-  return <>
-  <JumbotronContainer />
-  <FaqsContainer />
-  <FooterContainer />
-  </>
+  return (
+    <Router>
+    <JumbotronContainer />
+    <FaqsContainer />
+    <FooterContainer />
+    </Router>
+  )
+  
 }
 
