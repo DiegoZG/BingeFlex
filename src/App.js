165ed7d0297a9f542a7547abc6d3ea237741eab7
Diwo777 <diego.zegarra79@icloud.com>
2021-02-05 23:18:12 -0500

    added route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Router, Switch } from 'react-router-dom'
+import { BrowserRouter, Router, Switch, Route } from 'react-router-dom'
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
@@ -8,9 +8,11 @@ import * as ROUTES from './constants/routes'
 export default function App() {
   return (
     <Router>
+      <Route exact path="/signin">
     <JumbotronContainer />
     <FaqsContainer />
     <FooterContainer />
+    </Route>
     </Router>
   )
   
