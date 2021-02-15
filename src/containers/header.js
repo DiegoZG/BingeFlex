35a7acd05d1e14bd955da804bfbe1d0f852003af
Diwo777 <diego.zegarra79@icloud.com>
2021-02-15 15:49:51 -0500

    added styled components into header

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,5 +1,5 @@
 import React from 'react' 
-import { Accordion } from '../components'; 
+import { Header } from '../components'; 
 import { Header } from '../components/accordion/styles/accordion'
 
 export default function HeaderContainer() {
