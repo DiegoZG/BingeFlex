fa669b9646779cb965d06b92baf6cc598eb5beae
Diwo777 <diego.zegarra79@icloud.com>
2021-03-18 19:55:29 -0400

    Implemented Container

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,3 +3,7 @@ import React from 'react'
 export default function Header ({bg = true, children, ...restProps}) {
     return bg ? <Background { ...restProps}> {children}</Background>
 }
+
+Header.Frame = function HeaderFrame({children, ...restProps}) {
+    return <Container></Container>
+}
