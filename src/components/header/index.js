c7a0c731b254b004f6f6d14b8e954a35b8066fe9
Diwo777 <diego.zegarra79@icloud.com>
2021-03-21 13:27:36 -0400

    Added React Router Link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,4 +8,10 @@ Header.Frame = function HeaderFrame({children, ...restProps}) {
     return <Container {...restProps}> {children} </Container>
 }
 
-Header.Logo = function HeaderLogo({})
+Header.Logo = function HeaderLogo({ to, ...restProps}) {
+    return (
+        <ReactRouterLink to={to}>
+            
+        </ReactRouterLink>
+    )
+}
