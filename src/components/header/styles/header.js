21b2cc72ca0f84850e706ee5f69468e5dd71c2e2
Diwo777 <diego.zegarra79@icloud.com>
2021-03-24 19:35:54 -0400

    Created header styles

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,1 +1,7 @@
-import styled from 'styled-components/macro'
+import styled from "styled-components/macro";
+
+export const Background = styled.div``;
+
+export const Container = styled.div``;
+
+export const Frame = styled.div``;
