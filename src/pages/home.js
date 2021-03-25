464e2fd0af1a333f4374c1edc56b001bf257ec31
Diwo777 <diego.zegarra79@icloud.com>
2021-03-25 10:50:48 -0400

    Added HeaderContainer into home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,16 @@
-import React  from 'react';
-import { FaqsContainer } from '../containers/faqs';
-import { FooterContainer } from '../containers/footer';
-import { JumbotronContainer } from '../containers/jumbotron';
+import React from "react";
+import { FaqsContainer } from "../containers/faqs";
+import { FooterContainer } from "../containers/footer";
+import { JumbotronContainer } from "../containers/jumbotron";
+import { HeaderContainer } from "../containers/header";
 
 export default function Home() {
-    return (
-        <> 
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
-        
-        </>
-    )
+  return (
+    <>
+      <HeaderContainer />
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
 }
